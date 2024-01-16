e9e4049d403cfbed6520286368e32f53cba6ce2e
rpereira2Reloading <rpereira2@reloading.biz>
2024-01-16 19:31:05 +0000

    criação de models e da controller Character com funções GetAll

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RPG.Model;
+
+namespace RPG.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character {Id = 1 ,Name = "Sam"}
+        };
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<List<Character>> GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+    }
+}
